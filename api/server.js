be9767716b52f1dfe295737994693c816d5b5b57
Dale Pepper <dalepepper@Dales-MacBook-Pro.local>
2015-05-07 22:57:07 +0100

    Added model to server

--- api/server.js
+++ api/server.js
@@ -1,22 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var Post = require('./models/post');
+
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/api/posts', function(req, res){
-    res.json([
-        {
-            username: 'dale',
-            body: 'This is a test'
-        }
-    ])
-});
+var Post = require('./models/post');
 
-app.post('/api/posts', function(req, res){
-	console.log('post recieved');
-	console.log(req.body.username);
-	console.log(req.body.body);
+app.post('/api/posts', function(req, res, next){
+
+    var post = new Post({
+        username: req.body.username,
+        body: req.body.body
+    });
+
+    post.save(function (err, post){
+        if(err) { return next(err) }
+        res.json(201, post)
+    });
 	res.sendStatus(201);
 });
 
