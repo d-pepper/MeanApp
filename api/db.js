1c0dc021574ba530a7bb52318bf415779c1c9cbb
Dale Pepper <dalepepper@Dales-MacBook-Pro.local>
2015-12-03 23:38:18 +0000

    Commented out mongoose to debug

--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
-var mongoose = require('mongoose');
-
-mongoose.connect('mongo://localhost/meanappdb', function(){
-	console.log('mongodb connected');
-});
-
-module.exports = mongoose;
+//var mongoose = require('mongoose');
+//
+//mongoose.connect('mongo://localhost/meanappdb', function(){
+//	console.log('mongodb connected');
+//});
+//
+//module.exports = mongoose;
--- api/server.js
+++ api/server.js
@@ -1,26 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var Post = require('./models/post');
+//var Post = require('./models/post');
 
 var app = express();
 app.use(bodyParser.json());
 
-var Post = require('./models/post');
+app.post('/api/posts', function (req, res) {
+    console.log('post recieved!')
+    console.log(req.body.username)
+    console.log(req.body.body)
+    res.send(201)
+})
 
-app.post('/api/posts', function(req, res, next){
-
-    var post = new Post({
-        username: req.body.username,
-        body: req.body.body
-    });
-
-    post.save(function (err, post){
-        if(err) { return next(err) }
-        res.json(201, post)
-    });
-	res.sendStatus(201);
-});
+//app.post('/api/posts', function(req, res, next){
+//
+//    var post = new Post({
+//        username: req.body.username,
+//        body: req.body.body
+//    });
+//
+//    post.save(function (err, post){
+//        if(err) { return next(err) }
+//        res.json(201, post)
+//    });
+//});
 
 app.listen(3000, function(){
    console.log('Server listening on', 3000);
