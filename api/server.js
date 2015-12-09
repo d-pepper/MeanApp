dceba21328a8bc737a18e0849e0285a370004623
Dale Pepper <dalepepper@Dales-MacBook-Pro.local>
2015-12-09 23:45:30 +0000

    Partially resolved mongoose error

--- api/server.js
+++ api/server.js
@@ -1,31 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-//var Post = require('./models/post');
-
 var app = express();
 app.use(bodyParser.json());
 
-app.post('/api/posts', function (req, res) {
-    console.log('post recieved!')
-    console.log(req.body.username)
-    console.log(req.body.body)
-    res.send(201)
-})
+var Post = require('./models/post');
 
-//app.post('/api/posts', function(req, res, next){
-//
-//    var post = new Post({
-//        username: req.body.username,
-//        body: req.body.body
-//    });
-//
-//    post.save(function (err, post){
-//        if(err) { return next(err) }
-//        res.json(201, post)
-//    });
-//});
+app.post('/api/posts', function(req, res, next){
+
+    var post = new Post({
+        username: req.body.username,
+        body: req.body.body
+    });
+
+    post.save(function (err, post){
+        if(err) { return next(err) }
+        res.json(201, post)
+    });
+});
 
 app.listen(3000, function(){
    console.log('Server listening on', 3000);
 });
+
+//curl -v -H "Content-Type: application/json" -XPOST --data "{\"username\":\"dale\", \"body\":\"test test\"}" localhost:3000/api/posts
